51e6d8d526fb16ccfaf7437faaee71f77a06818c
Aaron White <aawhite@MATL2232LAWHITE.local>
2018-09-26 15:29:34 -0400

    Add hover functionality to objects.

--- content.js
+++ content.js
@@ -1,2 +1,26 @@
 
-$('body').css('background-color', 'salmon');
+$("[data-uuid]").mouseenter(function() {
+    $("[data-uuid], [data-objectuuid]").css('outline', 'unset');
+    $(this).css('outline', '10px solid rgba(100, 255, 100, .5)');
+}).mouseleave(function(){
+    $("[data-uuid], [data-objectuuid]").css('outline', 'unset');
+});
+
+$("[data-objectuuid]").mouseenter(function() {
+    $("[data-uuid], [data-objectuuid]").css('outline', 'unset');
+    $(this).css('outline', '10px solid rgba(255, 255, 100, .5)');
+});
+
+$("[data-objectuuid]").click(function() {
+    let uuid = $(this).closest("[data-objectuuid]").data('objectuuid'),
+        swingLink = 'http://10.243.230.4:5801/swing/index.html?null#open/' + uuid;
+    window.open(swingLink);
+    return false;
+});
+
+$("[data-uuid]").click(function() {
+    let uuid = $(this).closest("[data-uuid]").data('uuid'),
+        swingLink = 'http://10.243.230.4:5801/swing/index.html?null#open/' + uuid;
+    window.open(swingLink);
+    return false;
+});
